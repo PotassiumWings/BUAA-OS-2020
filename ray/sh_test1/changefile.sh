bc3a562078c59a166f42820622012e971bdba381
Potassium <mumuyuyululu@126.com>
2020-03-02 14:06:08 +0000

    first commit

--- ray/sh_test1/changefile.sh
+++ ray/sh_test1/changefile.sh
@@ -2,12 +2,12 @@
 a=1
 while [ $a -le 100 ]
 do
-	if [ $a -gt 70 ]	   #if loop variable is greater than 70
+	if [ $a -gt 70 ]
 	then
-
-	elif [ $a -gt 40 ]         # else if loop variable is great than 40   
+		rm -r file$a
+	elif [ $a -gt 40 ]
 	then
-	
+		mv file$a newfile$a
 	fi
-	           #don't forget change the loop variable
+	a=$[$a+1]
 done
