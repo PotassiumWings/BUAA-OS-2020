32a06a98ec6ec60c6436322fdb1279ee4206a410
Potassium <mumuyuyululu@126.com>
2020-03-30 15:24:09 +0000

    update \

--- include/queue.h
+++ include/queue.h
@@ -179,7 +179,7 @@
                         LIST_NEXT((elm), field)->field.le_prev =                \
                                         (elm)->field.le_prev;                           \
                 *(elm)->field.le_prev = LIST_NEXT((elm), field);                \
-                (elm)->field.le_prev = NULL;
+                (elm)->field.le_prev = NULL;\
         } while (0)
 
 /*
