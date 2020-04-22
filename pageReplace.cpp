471169350b4066ceff25fdfb9abe30cda399dc05
Potassium <mumuyuyululu@126.com>
2020-04-22 00:42:42 +0000

    lru-13

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -2,51 +2,43 @@
 #define MAXPAGE 64
 #define PG 12
 #define getPage(x) (x >> PG)
-#pragma optimize(3)
-//#pragma optimize(2)
+#pragma GCC optimize("-O2")
+//#pragma GCC optimize(3)
 
-#define MAXQUEUELEN 512
+#define MAXQUEUELEN 500
 // (hd,tl]
-int queue[520], hd, tl; //, cir; // circulate queue
+int queue[512], hd, tl; // circulate queue
 char vis[524288]; // page in queue num
 char pos[524288]; // in p index
+//inline 
 void push_Q (int x) {
 	if (tl == MAXQUEUELEN) {
 		tl = 0;
-		// cir++;
 	} else {
 		++tl;
 	}
 	queue[tl] = x;
 	++vis[x];
 }
-int pop_Q_one () {
-	if (hd == MAXQUEUELEN) {
-		hd = 0;
-		// cir--;
-	} else {
-		++hd;
-	}
-	--vis[queue[hd]];
-	if (vis[queue[hd]]) return -1;
-	return pos[queue[hd]];
-}
 int pop_Q () {
-	//while ((cir && hd > tl) || (!cir && hd < tl)) {
 	while (1) {
+		int x;
 		// del an element that doesnt exist in p 
-		int x = pop_Q_one();
-		if (x != -1)
-			return x;
+		if (hd == MAXQUEUELEN) {
+			hd = 0;
+		} else {
+			++hd;
+		}
+		if (!--vis[queue[hd]]) 
+			return pos[queue[hd]];
 	}
-	// while(1);
 }
 void pageReplace (long* p, long pa) {
 	int pgnum = getPage(pa);
 	static int full = 0;
 	static int cnt = 0;
 
-	if (vis[pgnum] >= 6) {
+	if (vis[pgnum] >= 7) {
 		return;
 	}
 
