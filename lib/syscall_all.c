012210564ee781f3f07df911c336cc7e87e68891
Potassium <mumuyuyululu@126.com>
2020-04-28 11:15:09 +0000

    1

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -118,7 +118,7 @@ int sys_set_pgfault_handler(int sysno, u_int envid, u_int func, u_int xstacktop)
 	struct Env *env;
 	int ret;
     //printf("sys_set_pgfault_handler entered, envid:%d\n",envid);
-    if ((ret = envid2env(envid, &env, 1)) < 0) return ret;
+    if ((ret = envid2env(envid, &env, 0)) < 0) return ret;
     env->env_pgfault_handler = func;
     env->env_xstacktop = xstacktop;
 	return 0;
