946c2eeb538f65afd059ebdf026f21011ae5ac57
buaa g306 <you@example.com>
2020-05-11 12:44:15 +0800

    Initial lab5

--- user/fktest.c
+++ user/fktest.c
@@ -5,27 +5,26 @@ void umain()
 {
 	int a = 0;
 	int id = 0;
-    int i;
 
 	if ((id = fork()) == 0) {
 		if ((id = fork()) == 0) {
 			a += 3;
 
-			for (i=0;i<20;i++) {
+			for (;;) {
 				writef("\t\tthis is child2 :a:%d\n", a);
 			}
 		}
 
 		a += 2;
 
-		for (i=0;i<20;i++) {
+		for (;;) {
 			writef("\tthis is child :a:%d\n", a);
 		}
 	}
 
 	a++;
 
-	for (i=0;i<20;i++) {
+	for (;;) {
 		writef("this is father: a:%d\n", a);
 	}
 }
