8648c20ae093a1de843dd5f7b4c867b3e13a264f
Potassium <mumuyuyululu@126.com>
2020-06-05 13:30:34 +0000

    1

--- user/pipe.c
+++ user/pipe.c
@@ -85,7 +85,12 @@ _pipeisclosed(struct Fd *fd, struct Pipe *p)
 	// everybody left is what fd is.  So the other end of
 	// the pipe is closed.
 	int pfd,pfp,runs;
-	
+	do{
+		runs=env->env_runs;
+		pfd=pageref(fd);
+		pfp=pageref(p);
+	}while(runs!=env->env_runs);
+	return pfp==pfd;
 
 
 
@@ -121,7 +126,21 @@ piperead(struct Fd *fd, void *vbuf, u_int n, u_int offset)
 	struct Pipe *p;
 	char *rbuf;
 	
-
+	p=(struct Pipe*)fd2data(fd);
+	while(p->p_rpos==p->p_wpos){
+		if(_pipeisclosed(fd,p))return 0;
+		syscall_yield();
+	}
+	rbuf=vbuf;
+	for(i=0;i<n;i++){
+		while(p->p_rpos==p->p_wpos) {
+			if(i>0||_pipeisclosed(fd,p)) return i;
+			syscall_yield();
+		}
+		rbuf[i]=p->p_buf[p->p_rpos%BY2PIPE];
+		p->p_rpos++;
+	}
+	return n;
 
 	user_panic("piperead not implemented");
 //	return -E_INVAL;
@@ -140,6 +159,18 @@ pipewrite(struct Fd *fd, const void *vbuf, u_int n, u_int offset)
 	int i;
 	struct Pipe *p;
 	char *wbuf;
+
+	p=fd2data(fd);
+	wbuf=vbuf;
+	for(i=0;i<n;i++){
+		while(p->p_wpos-p->p_rpos==BY2PIPE){
+			if(_pipeisclosed(fd,p))return 0;
+			syscall_yield();
+		}
+		p->p_buf[p->p_wpos%BY2PIPE]=wbuf[i];
+		p->p_wpos++;
+	}
+	return n;
 	
 
 //	return -E_INVAL;
