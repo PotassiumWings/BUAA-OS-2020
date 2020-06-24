7a9f02abea804ab4964eb4a87ff496f6f00c4253
Potassium <mumuyuyululu@126.com>
2020-06-24 02:49:04 +0000

    1

--- user/dev_cons.c
+++ user/dev_cons.c
@@ -1,6 +1,23 @@
 #include "lib.h"
 
 
+void u_myoutput(void *arg, const char *s, int l) {
+	int i;
+
+	// special termination call
+	if ((l == 1) && (s[0] == '\0')) {
+		return;
+	}
+
+	for (i = 0; i < l; i++) {
+		syscall_write_dev(&s[i],0x10000000,1);
+
+		if (s[i] == '\n') {
+			syscall_putchar(&s[i],0x10000000,1);
+		}
+	}
+}
+
 int ugetStr(char *buff) {
 	char c = 0;
 	int now_index = 0;
@@ -18,21 +35,4 @@ void uwritef(char *fmt, ...){
 	va_start(ap, fmt);
 	user_lp_Print(u_myoutput,0,fmt,ap);
 	va_end(ap);
-}
-
-void u_myoutput(void *arg, const char *s, int l) {
-	int i;
-
-	// special termination call
-	if ((l == 1) && (s[0] == '\0')) {
-		return;
-	}
-
-	for (i = 0; i < l; i++) {
-		syscall_write_dev(&s[i],0x10000000,1);
-
-		if (s[i] == '\n') {
-			syscall_putchar(&s[i],0x10000000,1);
-		}
-	}
 }
